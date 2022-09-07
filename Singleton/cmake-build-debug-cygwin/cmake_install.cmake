b10a9dbe5eb17054f510af1da8e0c559d2d9da4f
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-07 14:11:31 +0200

    googletest config

--- Singleton/cmake-build-debug-cygwin/cmake_install.cmake
+++ Singleton/cmake-build-debug-cygwin/cmake_install.cmake
@@ -37,6 +37,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump.exe")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/cygdrive/c/Users/Zosia/Desktop/Object-Oriented Design Patterns/Singleton/cmake-build-debug-cygwin/googletest-master/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
