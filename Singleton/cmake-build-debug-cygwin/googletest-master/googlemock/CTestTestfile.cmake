76596cec8f06b3936a2a9823bdb4984c5db9fb15
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-07 14:12:59 +0200

    project structure directories

---
+++ Singleton/cmake-build-debug-cygwin/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/Zosia/Desktop/Object-Oriented Design Patterns/Singleton/googletest-master/googlemock
+# Build directory: /cygdrive/c/Users/Zosia/Desktop/Object-Oriented Design Patterns/Singleton/cmake-build-debug-cygwin/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ Singleton/cmake-build-debug-cygwin/googletest-master/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/Zosia/Desktop/Object-Oriented Design Patterns/Singleton/googletest-master/googletest
+# Build directory: /cygdrive/c/Users/Zosia/Desktop/Object-Oriented Design Patterns/Singleton/cmake-build-debug-cygwin/googletest-master/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
