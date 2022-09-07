df334272547e8a9f4dec816f2afb37946c02414c
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-07 14:33:23 +0200

    first src and header files

---
+++ Singleton/include/file.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Zosia on 07.09.2022.
+//
+
+#ifndef SINGLETON_FILE_HPP
+#define SINGLETON_FILE_HPP
+
+#endif //SINGLETON_FILE_HPP
---
+++ Singleton/src/file.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Zosia on 07.09.2022.
+//
+
