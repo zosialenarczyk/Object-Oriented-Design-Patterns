75301268427173abe3132eeaf747f439adb0f4d1
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-08 12:06:20 +0200

    create files

---
+++ Strategy/include/duck.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Zosia on 08.09.2022.
+//
+
+#ifndef STRATEGY_DUCK_HPP
+#define STRATEGY_DUCK_HPP
+
+#endif //STRATEGY_DUCK_HPP
---
+++ Strategy/test/main_gtest.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Zosia on 08.09.2022.
+//
+
