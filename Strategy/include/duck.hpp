fc473d48d7fab8425f1522fd2198ddd3ba50c1e5
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-09 14:54:42 +0200

    small changes to create correct constructors and destructors

--- Strategy/include/duck.hpp
+++ Strategy/include/duck.hpp
@@ -5,8 +5,15 @@
 #ifndef STRATEGY_DUCK_HPP
 #define STRATEGY_DUCK_HPP
 
+#include <cstdlib>
+#include <memory>
+
+
+// ---------- STRATEGIES (INTERFACES) ---------------------
+
 class IFlyBehaviour {
 public:
+    IFlyBehaviour() = default;
     virtual bool fly() const = 0;
 };
 
@@ -19,6 +26,10 @@ class IDisplayBehaviour {
 public:
     virtual bool display() const = 0;
 };
+
+// ------------- CONCRETE CLASSES ---------------
+
+// IFlyBehaviour implementations ----------------
 
 class SimpleFly : public IFlyBehaviour {
 public:
@@ -35,6 +46,8 @@ public:
     bool fly() const override { return false; }
 };
 
+// IQuackBehaviour implementations ---------------
+
 class LoudQuack : public IQuackBehaviour {
 public:
     bool quack() const override { return true; }
@@ -49,6 +62,8 @@ class NoQuack : public IQuackBehaviour {
 public:
     bool quack() const override { return false; }
 };
+
+// IDisplayBehaviour implementations ---------------
 
 class TextDisplay : public IDisplayBehaviour {
 public:
@@ -65,21 +80,24 @@ public:
     bool display() const override { return false; }
 };
 
+// ------------ CONTEXT CLASS -----------------
+
 class Duck {
 public:
 
-    Duck(IFlyBehaviour fb, IQuackBehaviour qb, IDisplayBehaviour db) : fb_(fb), qb_(qb), db_(db) {}
+    Duck(IFlyBehaviour fb, IQuackBehaviour qb, IDisplayBehaviour db){}
+//    : fb_(std::move(fb), qb_(std::move(qb), db_(std::move(db))) ){}
 
-    bool fly() { fb_.fly(); }
-    bool quack() { qb_.quack(); }
-    bool display() { db_.display(); }
+    bool fly() { fb_->fly(); }
+    bool quack() { qb_->quack(); }
+    bool display() { db_->display(); }
 
     ~Duck(){}
 
 private:
-    IFlyBehaviour fb_;
-    IQuackBehaviour qb_;
-    IDisplayBehaviour db_;
+    std::unique_ptr<IFlyBehaviour> fb_;
+    std::unique_ptr<IQuackBehaviour> qb_;
+    std::unique_ptr<IDisplayBehaviour> db_;
 };
 
 
