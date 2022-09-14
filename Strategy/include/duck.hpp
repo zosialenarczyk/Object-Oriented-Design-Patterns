a1b9f20cfbfc39c7438b7d02a4b57fb13ddb3149
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-09-14 15:08:26 +0200

    Duck class contructor, setters

--- Strategy/include/duck.hpp
+++ Strategy/include/duck.hpp
@@ -13,7 +13,6 @@
 
 class IFlyBehaviour {
 public:
-    IFlyBehaviour() = default;
     virtual bool fly() const = 0;
 };
 
@@ -27,66 +26,72 @@ public:
     virtual bool display() const = 0;
 };
 
-// ------------- CONCRETE CLASSES ---------------
-
-// IFlyBehaviour implementations ----------------
-
-class SimpleFly : public IFlyBehaviour {
-public:
-    bool fly() const override { return true; }
-};
-
-class JetFly : public IFlyBehaviour {
-public:
-    bool fly() const override { return true; }
-};
-
-class NoFly : public IFlyBehaviour {
-public:
-    bool fly() const override { return false; }
-};
-
-// IQuackBehaviour implementations ---------------
-
-class LoudQuack : public IQuackBehaviour {
-public:
-    bool quack() const override { return true; }
-};
-
-class QuietQuack : public IQuackBehaviour {
-public:
-    bool quack() const override { return true; }
-};
-
-class NoQuack : public IQuackBehaviour {
-public:
-    bool quack() const override { return false; }
-};
-
-// IDisplayBehaviour implementations ---------------
-
-class TextDisplay : public IDisplayBehaviour {
-public:
-    bool display() const override { return true; }
-};
-
-class GraphicDisplay : public IDisplayBehaviour {
-public:
-    bool display() const override { return true; }
-};
-
-class NoDisplay : public IDisplayBehaviour {
-public:
-    bool display() const override { return false; }
-};
+//// ------------- CONCRETE CLASSES ---------------
+//
+//// IFlyBehaviour implementations ----------------
+//
+//class SimpleFly : public IFlyBehaviour {
+//public:
+//    bool fly() const override { return true; }
+//};
+//
+//class JetFly : public IFlyBehaviour {
+//public:
+//    bool fly() const override { return true; }
+//};
+//
+//class NoFly : public IFlyBehaviour {
+//public:
+//    bool fly() const override { return false; }
+//};
+//
+//// IQuackBehaviour implementations ---------------
+//
+//class LoudQuack : public IQuackBehaviour {
+//public:
+//    bool quack() const override { return true; }
+//};
+//
+//class QuietQuack : public IQuackBehaviour {
+//public:
+//    bool quack() const override { return true; }
+//};
+//
+//class NoQuack : public IQuackBehaviour {
+//public:
+//    bool quack() const override { return false; }
+//};
+//
+//// IDisplayBehaviour implementations ---------------
+//
+//class TextDisplay : public IDisplayBehaviour {
+//public:
+//    bool display() const override { return true; }
+//};
+//
+//class GraphicDisplay : public IDisplayBehaviour {
+//public:
+//    bool display() const override { return true; }
+//};
+//
+//class NoDisplay : public IDisplayBehaviour {
+//public:
+//    bool display() const override { return false; }
+//};
 
 // ------------ CONTEXT CLASS -----------------
 
 class Duck {
 public:
 
-    Duck(IFlyBehaviour fb, IQuackBehaviour qb, IDisplayBehaviour db){}
-//    : fb_(std::move(fb), qb_(std::move(qb), db_(std::move(db))) ){}
+    Duck() = default; // default constructor created with word "defualt" because compiler
+                    // will not create default constructor by itself when class has an user-implemented constructor
+    Duck(std::unique_ptr<IFlyBehaviour> fb, std::unique_ptr<IQuackBehaviour> qb, std::unique_ptr<IDisplayBehaviour> db); // default constructor
+
+    // setters
+    void set_fly(std::unique_ptr<IFlyBehaviour> fb) { fb_ = std::move(fb); }
+    void set_quack(std::unique_ptr<IQuackBehaviour> qb) { qb_ = std::move(qb); }
+    void set_display(std::unique_ptr<IDisplayBehaviour> db) { db_ = std::move(db); }
 
     bool fly() { fb_->fly(); }
     bool quack() { qb_->quack(); }
--- Strategy/src/duck.cpp
+++ Strategy/src/duck.cpp
@@ -2,3 +2,11 @@
 // Created by Zosia on 08.09.2022.
 //
 
+#include "duck.hpp"
+#include <memory>
+
+Duck::Duck(std::unique_ptr<IFlyBehaviour> fb, std::unique_ptr<IQuackBehaviour> qb, std::unique_ptr<IDisplayBehaviour> db) {
+    fb_ = std::move(fb);
+    qb_ = std::move(qb);
+    db_ = std::move(db);
+}
